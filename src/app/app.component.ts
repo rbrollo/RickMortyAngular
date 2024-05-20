2306ec6702fe86eab13439a5cf9df1075fa7374f
Rbrollo <rtbrollo@gmail.com>
2024-05-20 14:42:05 -0300

    initial header and hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BuscarPersonagemComponent } from './components/buscar-personagem/buscar-personagem.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BuscarPersonagemComponent],
+  imports: [RouterOutlet, BuscarPersonagemComponent,HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent implements OnInit {
+  ngOnInit(): void {}
+
+}
