091f7b531300a3b896237e5e59efe8365da9aeff
Rbrollo <rtbrollo@gmail.com>
2024-05-22 16:10:45 -0300

    adjustment in header + component card

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+  @Input() character: any;
+}
