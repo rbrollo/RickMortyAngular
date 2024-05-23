b1b24eaece2204616dc22442df0436127b9de48c
Rbrollo <rtbrollo@gmail.com>
2024-05-23 15:11:38 -0300

    adjustmentfor analytics vercel

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HeaderComponent } from './app/components/header/header.component';
+import { inject } from '@vercel/analytics';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
 
-bootstrapApplication(HeaderComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(HeaderComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+inject();
